38fe0b56d1f2aa2ec8f6933bf0fea0858391cfef
MadanKumar <madan739@gmail.com>
2020-06-28 22:05:45 +0530

    adding Beer Repository

---
+++ src/main/java/mk/springframework/mkbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package mk.springframework.mkbeerservice.repositories;
+
+import mk.springframework.mkbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
