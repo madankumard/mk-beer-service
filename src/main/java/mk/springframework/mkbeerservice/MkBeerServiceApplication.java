04a8f2c88eab8f3ece71a9a05849e1d83b0cd58f
MadanKumar <madan739@gmail.com>
2020-06-28 17:58:36 +0530

    Initial Project Commit

---
+++ src/main/java/mk/springframework/mkbeerservice/MkBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkBeerServiceApplication.class, args);
+	}
+
+}
