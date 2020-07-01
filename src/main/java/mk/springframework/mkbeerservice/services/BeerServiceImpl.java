e1b06ce95dff7d1a633a2f45b6522d60ec8357d1
MadanKumar <madan739@gmail.com>
2020-07-01 08:44:52 +0530

    adding ehcache

--- src/main/java/mk/springframework/mkbeerservice/services/BeerServiceImpl.java
+++ src/main/java/mk/springframework/mkbeerservice/services/BeerServiceImpl.java
@@ -1,6 +1,7 @@
 package mk.springframework.mkbeerservice.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mk.springframework.mkbeerservice.domain.Beer;
 import mk.springframework.mkbeerservice.repositories.BeerRepository;
 import mk.springframework.mkbeerservice.web.controller.NotFoundException;
@@ -8,6 +9,7 @@ import mk.springframework.mkbeerservice.web.mappers.BeerMapper;
 import mk.springframework.mkbeerservice.web.model.BeerDto;
 import mk.springframework.mkbeerservice.web.model.BeerPagedList;
 import mk.springframework.mkbeerservice.web.model.BeerStyleEnum;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import org.springframework.util.StringUtils;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BeerServiceImpl implements BeerService {
@@ -23,9 +26,13 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle,
                                    PageRequest pageRequest, Boolean showInventoryOnHand) {
+
+        log.debug("Called BeerServiceImpl.listBeers...");
+
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
         if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
@@ -66,8 +73,12 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+
+        log.debug("Called BeerServiceImpl.getById...");
+
         if (showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(
                     beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
