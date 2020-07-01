b9627d9153d71c315118fe16dd0817f79025bec7
MadanKumar <madan739@gmail.com>
2020-07-01 08:27:05 +0530

    adding showInventoryOnHand param

--- src/main/java/mk/springframework/mkbeerservice/services/BeerService.java
+++ src/main/java/mk/springframework/mkbeerservice/services/BeerService.java
@@ -8,8 +8,8 @@ import org.springframework.data.domain.PageRequest;
 import java.util.UUID;
 
 public interface BeerService {
-    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
-    BeerDto getById(UUID beerId);
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand);
+    BeerDto getById(UUID beerId, Boolean showInventoryOnHand);
     BeerDto saveNewBeer(BeerDto beerDto);
     BeerDto updateBeer(UUID beerId, BeerDto beerDto);
 }
